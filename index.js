6d4fb8556a26df1e8b3a69e1493cf8e6de209760
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-07-18 16:53:12 +0100

    parameterize

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 var moment = require('moment');
 
+var userArgs = process.argv.slice(2);
+var hours = userArgs.shift(userArgs) || 2;
+var mins = userArgs.shift(userArgs) || 0;
+
 var now = moment();
-var someOClock = moment().hour(2).startOf('hour');
+var someOClock = moment().hour(hours).minute(mins).startOf('minute');
 while(someOClock.isBefore(now)) {
 	someOClock.add(1, 'days');
 }
